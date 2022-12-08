4d554ebfa973649aead32acd33c50d7b2ca895dd
anastasia <nastay.dr2002@gmail.com>
2022-12-08 18:39:55 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,122 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9option1();
+        task9option2();
+        task10();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
+                System.out.println(i + " год является високосным");
+            }
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // чтобы перенести "Задача 7" на следующую строку
+    }
+
+
+    public static void task7() {
+        System.out.println("Задача 7");
+
+        System.out.println("Вариант 1");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // чтобы перенести "Вариант 2" на следующую строку
+
+        System.out.println("Вариант 2");
+        int number = 1;
+        System.out.print(number + " ");
+        for (int i = 0; i < 9; i++) {
+            number = number * 2;
+            System.out.print(number + " ");
+        }
+        System.out.println(); // чтобы перенести "Задача 8" на следующую строку
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int sum = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + sum;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task9option1 () {
+        System.out.println("Задача 9");
+        System.out.println("Вариант 1");
+        int sum = 29000;
+        double total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + total / 100;
+            total = total + sum;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task9option2 () {
+        System.out.println("Вариант 2");
+        int sum = 29000;
+        double total = 0;
+        double percent = 0.01;
+        for (int i = 1; i <= 12; i++) {
+            total = (total + sum) + (total * percent);
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task10 () {
+        System.out.println("Задача 10");
+        int two = 2;
+        for (int i = 1; i <= 10; i++) {
+            int multiplication = two * i;
+            System.out.println(two + "*" + i + "=" + multiplication);
+        }
     }
 }
